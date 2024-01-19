96c92daa9b12e156e56ccf1a49cc338687a6fb39
ddzy <1766083035@qq.com>
2024-01-19 13:47:10 +0800

    wip: 优化代码

--- patches/flutter_highlighter-0.1.1-hacking/test/flutter_highlight_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_highlighter/flutter_highlighter.dart';
-
-void main() {
-  test('adds one to input values', () {
-    //
-  });
-}
